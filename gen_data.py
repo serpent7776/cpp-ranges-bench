70aec93557de0243b08976ba60fb4ef06d9db186
Serpent7776 <Serpent7776@gmail.com>
2024-02-26 20:35:26 +0100

    Python code to generate fake data

---
+++ gen_data.py
@@ -0,0 +1,22 @@
+import csv
+from faker import Faker
+import random
+
+fake = Faker()
+
+NUM_ROWS = 10000
+
+with open('data.csv', 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+
+    csv_writer.writerow(['Id', 'Username', 'Connected Names'])
+
+    id = 1
+    for _ in range(NUM_ROWS):
+        username = fake.user_name()
+
+        num_connected_names = random.randint(1,  7)
+        connected_names = [fake.user_name() for _ in range(num_connected_names)]
+
+        csv_writer.writerow([id, username, *connected_names])
+        id += 1
